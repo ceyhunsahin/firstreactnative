bcca88ae4be31845b40863c3715c0d1e9a6ad56f
ceyhunsahin <67160612+ceyhunsahin@users.noreply.github.com>
2020-11-08 16:25:14 +0100

    regroupment questions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 import 'react-native-gesture-handler'
 import {AppRegistry} from 'react-native';
-import Router from './src/portfolio3/Router';
+import Router from './src/portfolio4/Router';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => Router );
